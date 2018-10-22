0b995c5cd2c98c9f2adbfc60f11bcbd245ecc7d5
Richard Jalonen <richard.jalonen@tptlive.ee>
2018-10-22 21:29:28 +0300

    First homework

---
+++ RiidelappKodutoo/HomeworkRJ/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HomeworkRJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Riidelapp riidelapp = new Riidelapp();
+            riidelapp.Laius = 25;
+            riidelapp.Pikkus = 20;
+            riidelapp.Varvus = "sinine";
+
+            Console.WriteLine(riidelapp);
+            Console.WriteLine($"Pindala: {ArvutaPindala(riidelapp)}");
+            riidelapp.PoolitaLapp();
+            Console.WriteLine($"Poolitatud lapp: {riidelapp}");
+            Console.WriteLine($"Uus poolitatud lapp: {riidelapp.PoolitatudUusRiidelapp()}");
+            riidelapp.PoolitaLappProtsentuaalselt(75);
+            Console.WriteLine($"75% lapp: {riidelapp}");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Lisa käsklus lapi pindala arvutamiseks
+        /// </summary>
+        /// <param name="riidelapp">riidelapp, mida poolitada</param>
+        /// <returns>riidelapi pindala</returns>
+        private static double ArvutaPindala(Riidelapp riidelapp)
+        {
+            return riidelapp.Laius * riidelapp.Pikkus;
+        }
+    }
+}
---
+++ RiidelappKodutoo/HomeworkRJ/Riidelapp.cs
@@ -0,0 +1,58 @@
+﻿namespace HomeworkRJ
+{
+    /// <summary>
+    /// Koosta klass riidelapi andmete hoidmiseks: pikkus, laius, toon
+    /// </summary>
+    class Riidelapp
+    {
+        public double Pikkus { get; set; }
+        public double Laius { get; set; }
+        public string Varvus { get; set; }
+
+        /// <summary>
+        /// Lisa käsklus lapi andmete väljatrükiks
+        /// </summary>
+        /// <returns>riidelapi andmed</returns>
+        public override string ToString()
+        {
+            return $"[Pikkus: {Pikkus}, Laius: {Laius}, Varvus: {Varvus}]";
+        }
+
+        /// <summary>
+        /// Lisa meetod (alamprogramm) lapi poolitamiseks: pikem külg tehakse poole lühemaks.
+        /// </summary>
+        public void PoolitaLapp()
+        {
+            if (Laius > Pikkus)
+                Laius /= 2;
+            else
+                Pikkus /= 2;
+        }
+
+        /// <summary>
+        /// Poolitamise meetod lisaks algse lapi poolitamisele väljastab ka uue samasuguse algsest poole väiksema eksemplari.
+        /// </summary>
+        /// <returns>poolitatud riidelapp</returns>
+        public Riidelapp PoolitatudUusRiidelapp()
+        {
+            PoolitaLapp();
+            Riidelapp riidelapp = new Riidelapp();
+            riidelapp.Pikkus = Pikkus;
+            riidelapp.Laius = Laius;
+            riidelapp.Varvus = Varvus;
+            return riidelapp;
+        }
+
+        /// <summary>
+        /// Lisa teine poolitusmeetod, kus saab määrata, mitme protsendi peale lõigatakse pikem külg
+        /// </summary>
+        /// <param name="protsent">protsent, mille peale loigata</param>
+        public void PoolitaLappProtsentuaalselt(double protsent)
+        {
+            if (Laius > Pikkus)
+                Laius = protsent / 100 * Laius;
+            else
+                Pikkus = protsent / 100 * Pikkus;
+        }
+    }
+}
