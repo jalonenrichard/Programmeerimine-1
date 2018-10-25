3178a41407fc8a686498af4b7ec8a55e4d60bbb1
Richard Jalonen <richard.jalonen@tptlive.ee>
2018-10-25 17:37:37 +0300

    Fixed lag

--- BallsOnDesk2/BallForm.cs
+++ BallsOnDesk2/BallForm.cs
@@ -11,16 +11,16 @@ namespace Ball
     public partial class BallForm : Form
     {
         bool paused = false; // palli liikumise status, kas seisab või liigub
-        int scrW = Screen.PrimaryScreen.WorkingArea.Width;  // ekraani lajus
+        int scrW = Screen.PrimaryScreen.WorkingArea.Width; // ekraani lajus
         int scrH = Screen.PrimaryScreen.WorkingArea.Height; // ekraani kõrgus
-        double moveX = 0.0;   // horisontaal kiirus
-        double moveY = 0.0;   // vertikaal kiirus
-        double X = 0.0;       // palli Horisontal koordinat
-        double Y = 0.0;       // palli Vertikal koordinaal
+        double moveX = 0.0; // horisontaal kiirus
+        double moveY = 0.0; // vertikaal kiirus
+        double X = 0.0; // palli Horisontal koordinat
+        double Y = 0.0; // palli Vertikal koordinaal
         double gravity = 0.1; // "maa külgetõmbejõud"
-        Color col;            // palli värv  
-        Random rand;          // lokaalse juhuarvu objekt
-        int Diameter;         // palli diameeter
+        Color col; // palli värv  
+        Random rand; // lokaalse juhuarvu objekt
+        int Diameter; // palli diameeter
         private int algDiameeter; // palli algne diameeter
         private bool miinimumSuurus = false;
 
@@ -30,37 +30,39 @@ namespace Ball
             InitializeComponent();
             SetStyle(ControlStyles.AllPaintingInWmPaint, true);
             rand = new Random(rnd); // lokaalse juhuarvu genereerimis objekti
-            col = Color.FromArgb(rand.Next(16, 240), rand.Next(16, 240), rand.Next(16, 240)); // genereerime juhuskiku värvi
-            Diameter = rand.Next(30, 100);            // genereerime juhuskiku diametri
+            col = Color.FromArgb(rand.Next(16, 240), rand.Next(16, 240),
+                rand.Next(16, 240)); // genereerime juhuskiku värvi
+            Diameter = rand.Next(30, 100); // genereerime juhuskiku diametri
             algDiameeter = Diameter;
             scrW = Screen.PrimaryScreen.WorkingArea.Width; // saame kätte ja määrame ekraani lajuse
-            scrH = Screen.PrimaryScreen.WorkingArea.Height;// saame kätte ja määrame ekraani kõrguse
-            moveX = rand.NextDouble();  // genereerime juhuskiku palli horisontaal kiiruse
-            moveY = rand.NextDouble();  // genereerime juhuskiku palli vertikaal kiiruse
+            scrH = Screen.PrimaryScreen.WorkingArea.Height; // saame kätte ja määrame ekraani kõrguse
+            moveX = rand.NextDouble(); // genereerime juhuskiku palli horisontaal kiiruse
+            moveY = rand.NextDouble(); // genereerime juhuskiku palli vertikaal kiiruse
             // ekraani X=0 ja Y=0 koordinaat asub üleval vasakus servas
-            X = rand.NextDouble() * scrW;  // genereerime juhuskiku palli algseisu X koordinaadi
-            Y = rand.NextDouble() * scrH;  // genereerime juhuskiku palli algseisu Y koordinaadi
-
+            X = rand.NextDouble() * scrW; // genereerime juhuskiku palli algseisu X koordinaadi
+            Y = rand.NextDouble() * scrH; // genereerime juhuskiku palli algseisu Y koordinaadi
         }
 
         private void BallForm_Paint(object sender, PaintEventArgs e)
-        {   
+        {
             Width = Height = Diameter; // määrame kanti lajuse ja kõrguse kuhu me joonistame palli
-            Graphics g = e.Graphics;   // Loome graafilise objekti 
-            g.Clear(Color.Cyan);       // puhastave palli kanti
-            g.FillPie(new SolidBrush(col), 0, 0, Width - 1, Height - 1, 0, 360);    // Joonistame ringi täites palli jaoks genereeritud värviga
-            g.DrawArc(new Pen(Color.Black,2), 0, 0, Width - 1, Height - 1, 0, 360); // Joonistame musta värvi ringi serva
-        } 
+            Graphics g = e.Graphics; // Loome graafilise objekti 
+            g.Clear(Color.Cyan); // puhastave palli kanti
+            g.FillPie(new SolidBrush(col), 0, 0, Width - 1, Height - 1, 0,
+                360); // Joonistame ringi täites palli jaoks genereeritud värviga
+            g.DrawArc(new Pen(Color.Black, 2), 0, 0, Width - 1, Height - 1, 0,
+                360); // Joonistame musta värvi ringi serva
+        }
 
         public void Tick()
         {
-            if (!paused && !moving) 
-            {         
+            if (!paused && !moving)
+            {
                 moveY -= gravity; // muudame vertikaalset kiirust gravitatsiooni koefitsiendi võrra
-                
-                X += moveX;  // liigutame horisontaalselt palli
-                Y += moveY;  // liigutame vertikaalselt palli
-                Location = new Point((int)X, (int)Y);  // määrame palli asukoha täisarv koordinaatidega
+
+                X += moveX; // liigutame horisontaalselt palli
+                Y += moveY; // liigutame vertikaalselt palli
+                Location = new Point((int) X, (int) Y); // määrame palli asukoha täisarv koordinaatidega
 
                 if (Diameter == 30)
                 {
@@ -70,12 +72,9 @@ namespace Ball
                 //Vähenda palli suurust kui see just ei ole miinimum
                 if (Diameter > 30 && !miinimumSuurus)
                 {
-                    //this.Visible = false;
-                    Hide();
                     Diameter--;
                     Width = Height = Diameter;
-                    //this.Visible = true;
-                    Show();
+                    this.Refresh();
                     if (Diameter == 30)
                     {
                         miinimumSuurus = true;
@@ -85,12 +84,9 @@ namespace Ball
                 //Suurenda pall algsuurusele kui on jõudnud miinimumini
                 if (Diameter < algDiameeter && miinimumSuurus)
                 {
-                    //this.Visible = false;
-                    Hide();
                     Diameter++;
                     Width = Height = Diameter;
-                    //this.Visible = true;
-                    Show();
+                    this.Refresh();
                     if (Diameter == algDiameeter)
                     {
                         miinimumSuurus = false;
@@ -98,48 +94,55 @@ namespace Ball
                 }
 
                 //Check Collision
-                if (X < 0)          // kui läks vasakust äärest üle
+                if (X < 0) // kui läks vasakust äärest üle
                 {
-                    X = 0;          // Tõmbame palli ekraani piiridesse tagasi
+                    X = 0; // Tõmbame palli ekraani piiridesse tagasi
                     moveX = -moveX; // muudame horisontaal liikumis suunda vastupidiseks
-                    moveX *= 0.75;  // vähendame horisontaal liikumis kiirust 
-                    moveY *= 0.95;  // vähendame vertikaal liikumis kiirust 
-                }
-                if (X > Screen.PrimaryScreen.WorkingArea.Width - 1 - Width) // kui läks paremast äärest üle, arvestades palli diameetrit
-                {
-                    X = Screen.PrimaryScreen.WorkingArea.Width - 1 - Width; // Tõmbame palli ekraani piiridesse tagasi
-                    moveX = -moveX;  // muudame horisontaal liikumis suunda vastupidiseks
-                    moveX *= 0.75;   // vähendame horisontaal liikumis kiirust 
-                    moveY *= 0.95;   // vähendame vertikaal liikumis kiirust 
+                    moveX *= 0.75; // vähendame horisontaal liikumis kiirust 
+                    moveY *= 0.95; // vähendame vertikaal liikumis kiirust 
                 }
 
-                if (Y < 0)           // kui läks ülemisest äärest üle
+                if (X > Screen.PrimaryScreen.WorkingArea.Width - 1 - Width
+                ) // kui läks paremast äärest üle, arvestades palli diameetrit
                 {
-                    Y = 0;           // Tõmbame palli ekraani piiridesse tagasi
-                    moveY = -moveY;  // muudame horisontaal liikumis suunda vastupidiseks
-                    moveY *= 0.75;   // vähendame horisontaal liikumis kiirust 
-                    moveX *= 0.95;   // vähendame vertikaal liikumis kiirust 
+                    X = Screen.PrimaryScreen.WorkingArea.Width - 1 - Width; // Tõmbame palli ekraani piiridesse tagasi
+                    moveX = -moveX; // muudame horisontaal liikumis suunda vastupidiseks
+                    moveX *= 0.75; // vähendame horisontaal liikumis kiirust 
+                    moveY *= 0.95; // vähendame vertikaal liikumis kiirust 
                 }
-                if (Y > Screen.PrimaryScreen.WorkingArea.Height - 1 - Height) // kui läks alumisest äärest üle, arvestades palli diameetrit
+
+                if (Y < 0) // kui läks ülemisest äärest üle
+                {
+                    Y = 0; // Tõmbame palli ekraani piiridesse tagasi
+                    moveY = -moveY; // muudame horisontaal liikumis suunda vastupidiseks
+                    moveY *= 0.75; // vähendame horisontaal liikumis kiirust 
+                    moveX *= 0.95; // vähendame vertikaal liikumis kiirust 
+                }
+
+                if (Y > Screen.PrimaryScreen.WorkingArea.Height - 1 - Height
+                ) // kui läks alumisest äärest üle, arvestades palli diameetrit
                 {
                     Y = Screen.PrimaryScreen.WorkingArea.Height - 1 - Height; // Tõmbame palli ekraani piiridesse tagasi
-                    moveY = -moveY;  // muudame horisontaal liikumis suunda vastupidiseks
-                    moveY *= 0.8;    // vähendame horisontaal liikumis kiirust 
-                    moveX *= 0.95;   // vähendame vertikaal liikumis kiirust 
+                    moveY = -moveY; // muudame horisontaal liikumis suunda vastupidiseks
+                    moveY *= 0.8; // vähendame horisontaal liikumis kiirust 
+                    moveX *= 0.95; // vähendame vertikaal liikumis kiirust 
                 }
+
                 // kui 
                 // palli Horisontaal ja vertikaal kiirused on väiksed
                 // ja aeg sekundites ei oma 3-ga jagamisel jääki
                 // ja pall on ekraani alumisest servast mitte kõrgemal kui määratud kõrgus
-                if (Math.Abs(moveX) < 0.1 && Math.Abs(moveY) < 0.5 && DateTime.Now.Second % 3 == 0 && Y < Screen.PrimaryScreen.WorkingArea.Height - 1 - 40)
+                if (Math.Abs(moveX) < 0.1 && Math.Abs(moveY) < 0.5 && DateTime.Now.Second % 3 == 0 &&
+                    Y < Screen.PrimaryScreen.WorkingArea.Height - 1 - 40)
                 {
-                    Bounce();        // anname pallile uue liikumise suuna ja kiiruse
+                    Bounce(); // anname pallile uue liikumise suuna ja kiiruse
                 }
             }
         }
 
         bool moving = false;
         Point rel = new Point();
+
         private void BallForm_MouseUp(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
@@ -148,6 +151,7 @@ namespace Ball
                 moving = false;
             }
         }
+
         private void BallForm_MouseDown(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
@@ -157,28 +161,29 @@ namespace Ball
                 moving = true;
             }
         }
+
         private void BallForm_MouseMove(object sender, MouseEventArgs e)
         {
             if (moving)
             {
                 X = Cursor.Position.X - rel.X;
                 Y = Cursor.Position.Y - rel.Y;
-                
-                moveX += (X - Location.X)/2;
-                moveY += (Y - Location.Y)/2;
+
+                moveX += (X - Location.X) / 2;
+                moveY += (Y - Location.Y) / 2;
 
                 if (moveX > 2)
                     moveX = 2;
                 if (moveY > 2)
                     moveY = 2;
-                
+
                 if (paused)
                 {
                     moveX = 0;
                     moveY = 0;
                 }
-                
-                Location = new Point((int)X, (int)Y);
+
+                Location = new Point((int) X, (int) Y);
             }
         }
 
@@ -187,20 +192,23 @@ namespace Ball
             Close();
             Environment.Exit(0);
         }
+
         private void pauseToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
         {
             paused = pauseToolStripMenuItem.Checked;
         }
+
         private void stopToolStripMenuItem_Click(object sender, EventArgs e)
         {
             moveX = 0;
             moveY = 0;
         }
+
         private void bounceToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Bounce();
         }
-        
+
         public void Bounce() // anname pallile uue liikumise suuna ja kiiruse
         {
             moveX = (rand.NextDouble() + rand.NextDouble()) - 1;
