4e34f0dfddf1c8bc0e0cef262bbb77a2f26b7671
unknown <richard.jalonen@tptlive.ee>
2018-11-22 16:30:07 +0200

    21.11 tunnitoo

---
+++ writingtexttofiles/WritingTextToFiles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace WritingTextToFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            for (int j = 0; j < 1000; j++)
+            {
+                FileStream fileStream = new FileStream($"textFile{j}.txt", FileMode.OpenOrCreate,
+                    FileAccess.ReadWrite);
+                StreamWriter streamWriter = new StreamWriter(fileStream);
+                for (int i = 0; i < 1000; i++)
+                {
+                    streamWriter.WriteLine(rnd.Next(999999999));
+                }
+
+                streamWriter.Close();
+            }
+        }
+    }
+}
